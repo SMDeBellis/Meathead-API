327b27b1f42fb7ab72c09f58f7788b2b106791ad
Sean DeBellis <sdebellis462@gmail.com>
2015-04-03 03:21:23 +0000

    implement registration

--- resources/login/register.js
+++ resources/login/register.js
@@ -1,14 +1,15 @@
 var db = require('../../db');
 
 module.exports = function register(request, response){
-	var user_name = request.params.name;
-	var email = request.params.email;
-	//var password = request.params.password;
-	//console.log({user: user_name, email: email, password: password});
-
-	//db.connect();
-	//db.query("INSERT INTO users VALUES('" + user_name + "', '" + email + ", '" + password + "')");
-	//db.end();
-	response.send({ feet: 'footbath', name: user_name, email: email});
-
+	var data = JSON.parse(request.body);
+	// callback is not necessary as schema validation is handled prior to this function being called.
+	db.query("select user_name from users where user_name = ?", data['user_name'], function(err, results){
+		if(results.length == 0){
+			db.query("insert into users (user_name, email, password) values(?,?,?)", [data['user_name'], data['email'], data['pass_hash']]);
+			response.send({code: '200 OK'});
+		}
+		else{
+			response.send({code: '201 DB ERROR'});
+		}			
+	});
 };
