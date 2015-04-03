5fc544d579c95b55aaffebd9587d305edfe6ed72
Sean DeBellis <sdebellis462@gmail.com>
2015-04-03 01:28:41 +0000

    load data to database

--- resources/workouts/create.js
+++ resources/workouts/create.js
@@ -1,4 +1,11 @@
-module.exports = function createWorkout(request, response){
-	response.json({ dude: "sweet"});
+var db = require('../../db')
 
+module.exports = function createWorkout(request, response, next){
+	var data = JSON.parse(request.body);
+	db.query("INSERT INTO workout(user_id, day) Values(?,?)", [parseInt(data['user_id']), parseInt(data['day'])]);
+	console.log(data);
+	console.log(data['user_id']);
+	console.log(data['day']);
+	response.send({ code: '200 OK' })
+	next();
 };
