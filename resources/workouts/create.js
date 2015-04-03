a21d9cbf26b55f50eec0cf9ec21d0e60d0da5395
Sean DeBellis <sdebellis462@gmail.com>
2015-04-03 02:02:20 +0000

    refactor code

--- resources/workouts/create.js
+++ resources/workouts/create.js
@@ -2,10 +2,15 @@ var db = require('../../db')
 
 module.exports = function createWorkout(request, response, next){
 	var data = JSON.parse(request.body);
+	// callback is not necessary as schema validation is handled prior to this function being called.
 	db.query("INSERT INTO workout(user_id, day) Values(?,?)", [parseInt(data['user_id']), parseInt(data['day'])]);
+	
+	/*
+	//debugging data 
 	console.log(data);
 	console.log(data['user_id']);
 	console.log(data['day']);
-	response.send({ code: '200 OK' })
+	*/
+
 	next();
 };
