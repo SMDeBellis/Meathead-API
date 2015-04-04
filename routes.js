e533f6b62054babb581145f658cf786a9c0e55e9
Sean DeBellis <sdebellis462@gmail.com>
2015-04-04 03:19:40 +0000

    change routes for put on workouts

--- routes.js
+++ routes.js
@@ -5,16 +5,17 @@ var server = require('./server');
 var resources = require('./resources');
 var validator = require('./middleware/validator');
 var schemas = require('./schemas');
+
 //users
-server.get('/login', resources.login.verify);
-server.put('/register/:name/:email', validator(schemas.registerSchema), resources.login.register);
+server.put('/login', validator(schemas.loginSchema), resources.login.verify);
+server.put('/register', validator(schemas.registerSchema), resources.login.register);
 
 //workouts
-server.get('/workouts/:user', resources.workouts.list);//Done (needs testing)
-server.put('/workouts/new/:id',/* validator(schemas.workoutSchema) ,*/resources.workouts.create);
+server.get('/workouts/:id', resources.workouts.list);//Done (needs testing)
+server.put('/workouts/new', validator(schemas.workoutSchema) , resources.workouts.create);
 server.del('/workouts/remove/:workout', resources.workouts.del );
 
 //exercises
 server.get('/workouts/exercises', resources.exercises.list);
-server.put('/workouts/exercises/new/:exercises',/* validator(schemas.exerciseSchema) ,*/ resources.exercises.create);
+server.put('/workouts/exercises/new/:exercises', validator(schemas.exerciseSchema) , resources.exercises.create);
 server.del('/workouts/exercises/:exercise', resources.exercises.del);
