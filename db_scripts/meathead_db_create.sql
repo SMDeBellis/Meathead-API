dadb8647a35828eeda2de1871202ccde6d7a0194
Sean DeBellis <sdebellis462@gmail.com>
2015-04-11 03:49:31 +0000

    modified db script

--- db_scripts/meathead_db_create.sql
+++ db_scripts/meathead_db_create.sql
@@ -5,29 +5,39 @@ create table if not exists users (
     password varchar(255) not null,
     primary key (id)
 );
-create table if not exists workout (
-    id int not null auto_increment,
+
+create table if not exists workout(
+    id int not null AUTO_INCREMENT,
+    workout_name varchar(64) not null,
+    workout_id BINARY(16) not null, /*uuid*/
     user_id int not null,
-    day int not null,
-    workout_data date not null,
+    primary key (id, workout_id)
+);
+
+create table if not exists completed_workout(
+    workout_id int not null,
+    user_id int not null,
+    id  BINARY(16) not null, /*uuid*/
+    date_completed date not null,
     primary key (id),
-    foreign key(user_id)
+    foreign key (user_id)
         references users(id)
         on delete cascade
 );
 
-create table if not exists exercise (
-    id int not null auto_increment,
-    exercise_name varchar(255) not null,
-    workout_id int not null,
-    num_sets int not null,
-    weight int not null default 0,
+create table if not exists exercises(
+    id int not null AUTO_INCREMENT,
+    name varchar(64) not null,
     exercise_id int not null,
+    set_num int not null,
+    weight int not null,
+    completed_workout_id BINARY(16) not null, /*uuid*/
     primary key (id),
-    foreign key(workout_id) 
-        references workout(id)
+    foreign key (completed_workout_id)
+        references completed_workout(id)
         on delete cascade
 );
+
 
 create table if not exists exercise_list (
     id int not null auto_increment,
@@ -37,4 +47,15 @@ create table if not exists exercise_list (
 );
 
 
+create table if not exists workout_exercise_list_rel(
+    /*id int not null AUTO_INCREMENT,*/
+    workout_id BINARY(16) not null,
+    exercise_id int not null,
+    primary key (workout_id)
+    /*Need to figure out how to get this do work
+	so that the rows will delete if a workout is deleted*/
+    /*foreign key (workout_id)
+        references workout(workout_id)
+        on delete cascade*/
+);
 
