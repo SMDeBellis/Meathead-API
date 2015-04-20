310a21595f6fd024a24472e964fc99be9b9d3956
Sean DeBellis <sdebellis462@gmail.com>
2015-04-20 01:52:58 +0000

    update server

--- server.js
+++ server.js
@@ -7,7 +7,13 @@ var options = {
 	name: 'Meathead API'	
 };
 
+// initialize the server
 var server = restify.createServer(options);
+
+//parse incoming json data
+server.use(restify.bodyParser({
+	maxBodySize: 10 * 1024
+}));
 
 module.exports = server;
 
